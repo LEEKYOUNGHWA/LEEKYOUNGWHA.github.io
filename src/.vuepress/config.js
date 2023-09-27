fb34220cbb7c23faf0b104451ff893ef4771c19d
LEEKYOUNGHWA <rudghk5220@gmail.com>
2023-09-27 13:43:15 +0900

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'LeeKyounghwa BLOG',
+  title: 'leekyounghwa BLOG',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
